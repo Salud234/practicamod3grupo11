29862f5b6d7211b6720f1afb3ad35e6ad32a64f7
Salud234 <168209748+Salud234@users.noreply.github.com>
2025-10-17 16:58:52 +0000

    uy7

--- BDconfiguration/consultador.py
+++ BDconfiguration/consultador.py
@@ -1,1 +1,13 @@
 import psycopg2
+conexion = psycopg2.connect(
+    hots = "localhost",
+    port = "5432"
+    database = "credenciales",
+    user = "Admin",
+    password = "p4ssw0rdDB"
+)
+
+cursor = conexion.cursor()
+
+cursor.execute("SELEC * FROM usuarios")
+regsitros = cursor.fetchall()
